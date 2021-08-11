38b1f473641f810c9fea54e3aac56484d7704796
rajwinderb <68147202+rajwinderb@users.noreply.github.com>
2021-08-11 14:33:17 +0100

    Documenting the functions

--- propose_mode.py
+++ propose_mode.py
@@ -4,6 +4,13 @@ super_distances = {'driving': '2,205 km', 'walking': '2,188 km', 'bicycling': '2
 
 
 def str_to_float(distances):
+    """
+    Function to change the string distances in in the dictionary distances from the API and changes them into floats
+    without "km".
+
+    :param distances: distances as dictionary (as would be received from the function get_distance())
+    :return: distances as dictionary where the values are floats of the distance (km)
+    """
     for key, value in distances.items():
         value = value.replace(',', '')
         distances[key] = float(value[:-3])
@@ -11,6 +18,13 @@ def str_to_float(distances):
 
 
 def propose_modes(distances):
+    """
+    This function removes the "walking" mode, from the distances dictionary,if the distance of the journey is greater
+    than 5km as will no longer be a viable mode of transport.
+
+    :param distances: distances as dictionary (as would be received from the function get_distance())
+    :return: distances as dictionary only including the viable modes of transport
+    """
     distances = str_to_float(distances)
     if distances['walking'] > 5:
         del distances['walking']
