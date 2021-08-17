f573114f8ab6d352d1b1efcac6773dcc5a02a485
Robyn Seymour-Jones <robynfsj@gmail.com>
2021-08-17 17:46:09 +0100

    delete app.py as flask app not required

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request
-from db_utils import # import functions etc
-
-app = Flask(__name__)
-
-# File to make our API
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
