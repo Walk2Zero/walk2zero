3b02548799b3a2a2fbe45b634eff76da2538f741
rajwinderb <68147202+rajwinderb@users.noreply.github.com>
2021-08-20 12:29:02 +0100

    2 google maps api functions tested, mock input not working, but can still test.

--- tests/test_googlemaps.py
+++ tests/test_googlemaps.py
@@ -5,10 +5,20 @@ from functions.googlemaps import input_locations, get_distance
 
 class TestGoogleMaps(unittest.TestCase):
 
-    @mock.patch('builtins.input', side_effect=['L3 8EN', 'B3 3DH'])
+    @mock.patch('builtins.input', side_effect=["L3 8EN", "B3 3DH"])
     def test_input_locations(self, mock_input):
-        expected = 'L3 8EN', 'B3 3DH'
+        expected = "L3 8EN", "B3 3DH"
         result = input_locations()
+        self.assertEqual(expected, result)
+
+    def test_google_maps_api(self):
+        origin_address = 'Liverpool L3 8EN, UK'
+        destination_address = 'Birmingham B2 4QA, UK'
+        distances = {'driving': '161 km', 'walking': '143 km', 'bicycling': '154 km', 'transit': '143 km'}
+        durations = {'driving': '1 hour 57 mins', 'walking': '1 day 5 hours', 'bicycling': '8 hours 25 mins',
+                     'transit': '1 hour 53 mins'}
+        expected = origin_address, destination_address, distances, durations
+        result = get_distance('L3 8EN', 'B2 4QA')
         self.assertEqual(expected, result)
 
 
